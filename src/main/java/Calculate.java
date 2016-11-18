164393513ac44d0901f039ecb7a5113fdfa96793
Kosiński Artur <artur.kosinski@coi.gov.pl>
2016-11-18 10:29:25 +0100

    Init

---
+++ src/main/java/Calculate.java
@@ -0,0 +1,18 @@
+/**
+ * Created by akosinski on 2016-11-17.
+ */
+public class Calculate {
+
+    public int add(int a, int b) {
+
+        if (a > 0) {
+            return a + b;
+        }
+        return 0;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+}
---
+++ src/test/java/CalculateTest.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * Created by akosinski on 2016-11-17.
+ */
+public class CalculateTest {
+
+    @Test
+    public void add() throws Exception {
+        //given
+        //when
+        int actual = new Calculate().add(1, 2);
+
+        //then
+        Assert.assertEquals(actual, 3);
+    }
+}
